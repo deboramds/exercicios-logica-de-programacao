c772e2a90790e5d429928f0f1f358910f1dd537e
Débora Martins <109089485+deboramds@users.noreply.github.com>
2024-04-08 14:20:01 -0300

    solução exercício 4 da sessão 3 ( reverse3)

---
+++ session-03/exercises/reverse3/reverse.js
@@ -0,0 +1,10 @@
+// Dado um array de inteiros de comprimento 3, devolva um novo array com os
+// elementos em ordem inversa, de modo que `[1, 2, 3]` se torne `[3, 2, 1]`.
+
+
+
+// solução exercício 4 da sessão 3 ( reverse3)
+
+function reverse3(nums){
+    return nums.reverse();
+  }
