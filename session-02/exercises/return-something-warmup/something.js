43d41dae2e90e99da18ce211c584668f29d208ac
Débora Martins <109089485+deboramds@users.noreply.github.com>
2024-04-08 11:04:59 -0300

    solução exercício 3 da sessão 2 -( Return Something to Me)

---
+++ session-02/exercises/return-something-warmup/something.js
@@ -0,0 +1,8 @@
+// Neste exercício deverá criar uma função que retorne a string `"something"` unida
+// com um espaço `" "` e o argumento fornecido `a`.
+
+// Solução exercicio 3 sessão 2
+
+function giveMeSomething(a) {
+    return "something " + a;
+}
