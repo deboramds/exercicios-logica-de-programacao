0338bad1e1fb53dac6e0b66ff7a790bf50623b27
Débora Martins <109089485+deboramds@users.noreply.github.com>
2024-04-08 14:11:49 -0300

    solução exercicio 3 da sessão 3 - remove-string)

---
+++ session-03/exercises/remove-string-spaces/remove.js
@@ -0,0 +1,10 @@
+
+
+
+// Simples, remova os espaços da string e retorne a string resultante.
+
+// solução exercício 3 da sessão 3 (remove-string)
+
+function noSpace(x){
+    return x.replace(/\s/g, '');
+  }
