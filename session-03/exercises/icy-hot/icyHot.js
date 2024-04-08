1910b2b767ec1d8ecdb452063b175efbfb3c76fc
Débora Martins <109089485+deboramds@users.noreply.github.com>
2024-04-08 11:42:47 -0300

    correção exercicio 2 da sessão 3 (icyhot)

--- session-03/exercises/icy-hot/icyHot.js
+++ session-03/exercises/icy-hot/icyHot.js
@@ -3,7 +3,7 @@
 // Dadas duas temperaturas, o retorno é verdadeiro se uma for inferior a 0 e a
 // outra superior a 100.
 
-// Solução exercicio 1 sessão 03 (icyHot)
+// Solução exercicio 2 sessão 03 (icyHot)
 function icyHot(temp1, temp2){
     if(temp1 < 0 && temp2 > 100 || temp1 > 0 && temp2 < 100 )
       return true
