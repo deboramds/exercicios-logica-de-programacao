149dc44946a7e90b0a8b54d756399121bb94e01b
Débora Martins <109089485+deboramds@users.noreply.github.com>
2024-04-09 10:25:45 -0300

    Solução do exercício 5 da sessão 3 (secret-society)

---
+++ session-03/exercises/secret-society/secretSociety.js
@@ -0,0 +1,10 @@
+// Crie uma função que receba um array de nomes e retorne o nome da sociedade
+// secreta.
+
+
+// Solução do exercício 5 da sessão 3 (secret-society)
+
+
+function societyName(friends) {
+    return friends.map((friend) => friend[0]).sort().join('')
+    }
