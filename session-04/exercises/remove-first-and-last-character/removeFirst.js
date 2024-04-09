9315c73a1011f2654d7a2e4b5ea013423f389462
Débora Martins <109089485+deboramds@users.noreply.github.com>
2024-04-09 15:04:53 -0300

    solução exercício 6 da sessão 4 (remove-first and Last Character)

---
+++ session-04/exercises/remove-first-and-last-character/removeFirst.js
@@ -0,0 +1,10 @@
+// O objetivo é criar uma função que remova o primeiro e o último caractere da string.
+// A função tem como parâmetro uma string 'str'. Você não tem que se preocupar com
+// strings com menos de dois caracteres.
+
+// solução exercício 6 da sessão 4 (remove-first and Last Character)
+
+function removeChar(str){
+    return str.slice(1, -1);
+  };
+  
